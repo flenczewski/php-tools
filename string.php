120af7a8970084d4ff2d198efd52505df7a0cc60
Fabian <fabian.lenczewski@gmail.com>
2014-06-09 19:41:12 +0200

    Create string.php

---
+++ string.php
@@ -0,0 +1,15 @@
+class String
+{
+
+    public static function clickable($text)
+    {
+        $ret = ' ' . $text;
+        $ret = preg_replace("#(^|[\n ])([\w]+?://[\w]+[^ \"\n\r\t<]*)#ise", "'\\1<a href=\"\\2\" >\\2</a>'", $ret);
+        $ret = preg_replace("#(^|[\n ])((www|ftp)\.[^ \"\t\n\r<]*)#ise", "'\\1<a href=\"http://\\2\" >\\2</a>'", $ret);
+        $ret = preg_replace("#(^|[\n ])([a-z0-9&\-_\.]+?)@([\w\-]+\.([\w\-\.]+\.)*[\w]+)#i", "\\1<a href=\"mailto:\\2@\\3\">\\2@\\3</a>", $ret);
+        $ret = substr($ret, 1);
+
+        return $ret;
+    }
+
+}
