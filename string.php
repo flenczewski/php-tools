4e6dd3383c58d165b7e159c4eb9a6e1d71bf9189
flenczewski <fabian.lenczewski@gmail.com>
2014-06-11 22:25:16 +0200

    Update string.php

--- string.php
+++ string.php
@@ -12,4 +12,38 @@ class String
         return $ret;
     }
 
+
+    /**
+     * Zwraca sformatowaną wielkosc pliku
+     * 
+     * @return string
+     */
+    public static function viewfileSize($size, $precision=2, $long_name=true, $real_size=true)
+    {
+        $pos  = 0;
+        $base = $real_size ? 1024 : 1000;
+
+        while ($size > $base) {
+            $size/=$base;
+            $pos++;
+        }
+
+        $prefix = array(" ",
+                        "kilo",
+                        "mega",
+                        "giga",
+                        "tera",
+                        "peta",
+                        "exa",
+                        "zetta",
+                        "yotta",
+                        "xenna",
+                        "w-",
+                        "vendeka",
+                        "u-",
+                        "?-");
+
+        $size_name = $long_name ? $prefix[$pos] .'byte' : $prefix[$pos][0] .'B';
+        return round($size, $precision) .' '. ucfirst($size_name);
+    }
 }
